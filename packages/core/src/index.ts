61328dec9578fb9e5a25253c08d7db50ef3fe863
cyan <contact@cyans.me>
2024-02-24 18:25:27 +0800

    chore: update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -22,7 +22,7 @@ class ImageService extends Service {
   private sources: ImageSource[] = []
   private languageDetect = new LanguageDetect()
 
-  constructor(ctx: Context, config: Config) {
+  constructor(protected ctx: Context, protected config: Config) {
     super(ctx, 'booru', true)
     this.config = config
   }
